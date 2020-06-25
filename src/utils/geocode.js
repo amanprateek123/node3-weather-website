c0ad0680d181d9f2313bd325b6052fc8ca9aafdf
amanpra123 <amanpra333@gmail.com>
2020-06-25 15:14:05 +0530

    changing dependencies

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,10 +3,10 @@ const geoCode = (address,callback) =>{
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiYW1hbnByYTMzMyIsImEiOiJja2JudTVobjAxdXdnMzFteXFnM3czNGRjIn0.RVNJRowGECgtnfctD5h5hQ&limit=1"
     request({url,json:true},(error,{body})=>{
         if(error){
-            callback('Unable to connect to the location service!')
+            callback('Unable to connect to the location service!',undefined)
         }
         else if(body.message){
-            callback('Unable to find location. Try another search!')
+            callback('Unable to find location. Try another search!',undefined)
         }
         else{
             callback(undefined,{
